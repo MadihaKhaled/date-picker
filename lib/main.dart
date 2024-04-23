642d61869c142984a91021ed30c3bf9edb66989a
MadihaKhaled <madihakhaled912@gmail.com>
2024-04-23 06:47:07 +0200

    test2

--- lib/main.dart
+++ lib/main.dart
@@ -19,42 +19,70 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ListView(children: [
-          Card(
-            margin: EdgeInsets.all(10),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            child: ListTile(
-              onTap: (){},
-              title: Text("Mady",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-              subtitle: Text("Madiha Khaled",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
-              trailing: Text("25/9/2024",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
-              leading: Text("1",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
+        body: Container(
+          child: Column(children: [
+            Container(
+              margin: EdgeInsets.only(top: 30,left: 20,right: 20,),
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(color: Color(0xffe6f0fa),border: Border.all(color: Colors.black,width: 2)),
+              width: 400,
+              child: Text("Strawberry Pavolva",style: TextStyle(color: Colors.grey[600],fontWeight: FontWeight.bold,fontSize: 20),textAlign: TextAlign.center,),
             ),
-          ),
-          Card(
-            // test git hub
-            margin: EdgeInsets.all(10),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            child: ListTile(
-              onTap: (){},
-              title: Text("Hema",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-              subtitle: Text("Ebrahim Khaled",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
-              trailing: Text("25/9/2024",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
-              leading: Text("2",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
+        Container(
+          margin: EdgeInsets.only(top: 20,left: 20,right: 20, ),
+          padding: EdgeInsets.symmetric(horizontal: 35),
+          decoration: BoxDecoration(color: Color(0xffe6f0fa),border: Border.all(color: Colors.black,width: 2)),
+          width: 400,
+          child: Text("Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavolva. Pavlova features erisp erusl and soft,light inside,lopped with fruit and whipped cream.",
+            style: TextStyle(color: Colors.grey[500],fontWeight: FontWeight.bold,fontSize: 15),textAlign: TextAlign.center,),
+        ),
+            Container(
+              margin: EdgeInsets.only(top: 20,left: 20,right: 20, ),
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(color: Color(0xffe6f0fa),border: Border.all(color: Colors.black,width: 2)),
+              width: 400,
+              child: Padding(padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Row(
+                  children: [
+                  Icon(Icons.star,color: Colors.grey,size: 20,),
+                    Icon(Icons.star,color: Colors.grey,size: 20,),
+                    Icon(Icons.star,color: Colors.grey,size: 20,),
+                    Icon(Icons.star,color: Colors.grey,size: 20,),
+                    Icon(Icons.star,color: Colors.grey,size: 20,),
+                    SizedBox(width: 20,),
+                    Text("170 Reviews",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),)
+                ],),
+              )
             ),
-          ),
-          Card(
-            margin: EdgeInsets.all(10),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            child: ListTile(
-              onTap: (){},
-              title: Text("Rody",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-              subtitle: Text("Rodyna Khaled",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
-              trailing: Text("25/9/2024",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
-              leading: Text("3",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
+            Container(
+              margin: EdgeInsets.only(top: 20,left: 20,right: 20, ),
+              padding: EdgeInsets.symmetric(horizontal: 35),
+              decoration: BoxDecoration(color: Color(0xffe6f0fa),border: Border.all(color: Colors.black,width: 2)),
+              width: 400,
+              child: Padding(padding: EdgeInsets.symmetric(vertical: 10),
+                child: Row(mainAxisAlignment:MainAxisAlignment.spaceBetween,children: [
+                  Column(
+                    children: [
+                    Icon(Icons.book_outlined,color: Colors.lightGreen[200],),
+                    Text("PREP",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),),
+                    Text("25 min",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),)
+
+                  ],),
+                  Column(children: [
+                    Icon(Icons.timer_outlined,color:Colors.lightGreen[200]),
+                    Text("COOK",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),),
+                    Text("1 hr",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),)
+                  ],),
+                  Column(children: [
+                    Icon(Icons.restaurant_rounded,color:Colors.lightGreen[200]),
+                    Text("FEEDS",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),),
+                    Text("4-6",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),)
+                  ],)
+                ],),
+              )
             ),
-          )
-        ]),
+          ],),
+        )
       ),
     );
   }
